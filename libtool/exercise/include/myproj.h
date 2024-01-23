99d82d0ab661214f37c47004efa98a64f4352a30
Ray Kuo <ray20140811@gmail.com>
2024-01-23 17:14:14 +0800

    create libtool/exercise

---
+++ libtool/exercise/include/myproj.h
@@ -0,0 +1,8 @@
+#ifndef myproj_hh
+#define myproj_hh 1
+
+#define IMPORTANT_MACRO 1
+
+int my_lib_function();
+
+#endif
---
+++ libtool/exercise/src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "myproj.h"
+
+int main()
+{
+
+	my_lib_function();
+	return 0;
+}
+
+#ifdef IMPORTANT_MACRO
+int my_fcn()
+{
+
+}
+#endif
